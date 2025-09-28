9078f7f45842f1614c97c312a62a9d0057bcdd41
Tony Cheetham <Tony@Oddsphere.com>
2025-09-28 16:26:24 +0100

    Added "NoScript" script to assign base type to all main scene files

---
+++ Godot/Code/Platform/Screens/NoScript.cs
@@ -0,0 +1,9 @@
+using Deuteros.Code.Platform.Base;
+
+namespace Deuteros.Code.Platform.Screens
+{
+    public partial class NoScript : BaseSubScene
+    {
+        //Placeholder for scenes with no script
+    }
+}
